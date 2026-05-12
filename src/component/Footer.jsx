eb40aa80de877387842b463ab9391924fa136de3
NitinRana08 <nitinr8229@gmail.com>
2026-05-12 15:26:02 +0530

    .
    .

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,7 @@
-// import Facebook.png from "public/Footer/facebook.png";
 function Footer() {
   return (
     <footer className="bg-zinc-950 text-white px-6 py-16 border-t border-zinc-800">
-      
+
       <div className="max-w-6xl mx-auto">
 
         {/* Heading */}
@@ -19,40 +18,76 @@ function Footer() {
         {/* Social Icons */}
         <div className="flex justify-center gap-6 flex-wrap mb-12">
 
+          {/* Facebook */}
           <a
-            href="#"
-            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl text-2xl transition duration-300"
+            href="https://www.facebook.com/share/1HubgYp57P/?mibextid=wwXIfr"
+            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl transition duration-300"
           >
-           
-            <img src="/Footer/Facebook.png" alt="facebook" />
+            <img
+              src="/Facebook.png"
+              alt="facebook"
+              className="w-8 h-8 object-contain"
+            />
           </a>
 
+          {/* Instagram */}
           <a
-            href="#"
-            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl text-2xl transition duration-300"
+            href="https://www.instagram.com/nitinrana23_/"
+            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl transition duration-300"
           >
-            <img src="/Footer/instagram.png.avif" alt="" />
+            <img
+              src="/instagram.png"
+              alt="instagram"
+              className="w-8 h-8 object-contain"
+            />
           </a>
 
+          {/* YouTube */}
           <a
             href="#"
-            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl text-2xl transition duration-300"
+            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl transition duration-300"
           >
-            {/* <FaYoutube /> */}
+            <img
+              src="/youtube.png"
+              alt="youtube"
+              className="w-8 h-8 object-contain"
+            />
           </a>
 
+          {/* X / Twitter */}
           <a
             href="#"
-            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl text-2xl transition duration-300"
+            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl transition duration-300"
           >
-            {/* <FaXTwitter /> */}
+            <img
+              src="/X.png"
+              alt="twitter"
+              className="w-8 h-8 object-contain"
+            />
           </a>
 
+          {/* LinkedIn */}
           <a
             href="#"
-            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl text-2xl transition duration-300"
+            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl transition duration-300"
           >
-            {/* <FaLinkedin /> */}
+            <img
+              src="/linkedin.png"
+              alt="linkedin"
+              className="w-8 h-8 object-contain"
+            />
+          </a>
+
+          {/* Mail */}
+          <a
+            href="mailto:nitinr8229@gmail.com"
+            className="bg-zinc-900 hover:bg-red-600 p-4 rounded-2xl transition duration-300"
+          >
+            <img
+              src="/mail.png"
+              alt="mail"
+              className="w-8 h-8 object-contain"
+            />
           </a>
 
         </div>
